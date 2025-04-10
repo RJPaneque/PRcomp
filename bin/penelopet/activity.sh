1171a583504cbeacdb7d03be085f5e84e8456eb7
RJPaneque <rpaneque@ucm.es>
2025-04-10 09:37:19 +0200

    implemented error handling

--- bin/penelopet/activity.sh
+++ bin/penelopet/activity.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 declare act="$1"      # e.g. 1e6, 100, 9.3e6
 
+if [ -z "$act" ]; then
+    echo "Usage: $0 <activity>"
+    echo "Example: $0 1e6"
+    exit 1
+fi
+
 # PENELOPET: change activity for the 1sec simulation, number of histories for different times
 #-->WARNING: if simulation time is changed, must change $act -> $act/t in following line
 declare nhist=$act
