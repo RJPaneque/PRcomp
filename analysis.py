11dcf1b5faaf288f6188dae0975ccff516f6d89e
RJPaneque <rpaneque@ucm.es>
2025-04-20 19:47:08 +0200

    bug from last commit fixed (overwrote fmt)

--- analysis.py
+++ analysis.py
@@ -188,11 +188,10 @@ class GetResults:
 
         #plt.figure()
         for label, result in results.items():
+            f = fmt[label] if fmt and type(fmt) == dict else fmt
             if sublabels:
                 label = sublabels[label]
-            if fmt and type(fmt) == dict:
-                fmt = fmt[label]
-            plt.plot(eval(xQoI+'*10'), eval(yQoI), fmt, label=label) # *10 to convert to mm
+            plt.plot(eval(xQoI+'*10'), eval(yQoI), f, label=label) # *10 to convert to mm
         
         if log_scale: plt.yscale('log')
 
